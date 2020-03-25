997c38d6f0671b25622efa3b855f0c2d891e8642
Joaquin Noguera <nvjoaquien@gmail.com>
2020-03-25 14:19:22 -0300

    ligeras adaptaciones para celular

--- src/index.js
+++ src/index.js
@@ -69,135 +69,6 @@ function App(){
         },
     }
 
-    function rotate(arr, dir,order){
-        let rotated = [];
-        let tempArray = [...arr];
-        switch(dir){
-            case 'R': {
-                for(let i = 0; i < order; i++){         
-                    let el = Math.pow(4,i);
-                    for(let j = 0; j < tempArray.length; j = j + el * 4){
-                            tempArray.intercalate(j,j + el * 3,el);
-                    }  
-                }
-
-
-                tempArray.forEach((el) => {
-                    rotated.push(directions[el].rotateRigth);
-                })
-                break; 
-            }
-
-            case 'L': {
-                const cant = tempArray.length;
-                for(let i = 0; i < order; i++){         
-                    let el = Math.pow(4,i);
-                    for(let j = 0; j < cant; j = j + el * 4){
-                            tempArray.reverseBlock(j,el);
-                    }  
-                }
-
-                tempArray.forEach((el) => {
-                rotated.push(directions[el].rotateLeft);
-                });
-                break;
-            }
-        }
-        return rotated;
-
-    }
-
-    function makeLeters(order, first){
-        let arr = [first];
-        for(let i = 1; i <= order; i++){
-            const temp = arr;
-            arr = [];
-            switch(first){
-                case 'R': {
-
-                }
-                case 'L': {
-
-                }
-                case 'U': {
-
-                }
-                case 'D': {
-                    arr = [...rotate(temp, 'R',i - 1)]
-                    arr = [...arr, ...temp]
-                    arr = [...arr, ...temp]
-                    arr = [...arr, ...rotate(temp, 'L',i - 1)]
-                }    
-
-            }
-        }
-        return arr;
-    }
-    
-    function makePoint(arr,size,dx,dy){
-        let arrayPoint = [];
-        if(order === 0) {
-            directions[arr[0]].points.forEach(
-                                        el=>arrayPoint.push(
-                                                new Point(dx + el[0] * size, dy + el[1] * size)
-                                                )
-                                    )
-            return arrayPoint; 
-            
-        }
-        
-        const lengthBin = (arr.length - 1).toString(2).length;
-        console.log(arr);
-        for(let i = 0; i < arr.length; i++){
-            
-            let newSize = size;
-            const number = i.toString(2).padStart(lengthBin,"0");
-            let x = 0;
-            let y = 0;
-            let count = 0;
-            let block = arr.length;
-
-            for(let j = order; j > 0; j--){
-                let comb = number.substr(count,2);
-                newSize = newSize * 0.5;
-                count = count + 2;
-                let actualBlock = parseInt(i/block);
-                let indexLeter = actualBlock * block;
-              console.log(indexLeter);
-                const letter = 'D';
-                switch(comb){
-                    case '00':{
-                        x+= newSize * directions[letter].points[0][0];
-                        y+= newSize  * directions[letter].points[0][1];
-                        break;
-                    }
-                    case'01':{
-                        x+= newSize * directions[letter].points[1][0];
-                        y+= newSize  * directions[letter].points[1][1];
-                        break;
-                    }
-                    case '10':{
-                        x+= newSize * directions[letter].points[2][0];
-                        y+= newSize  * directions[letter].points[2][1];
-                        break;
-                    }
-                    case '11':{
-                        x+= newSize * directions[letter].points[3][0];
-                        y+= newSize  * directions[letter].points[3][1];
-                        break;
-                    }
-                }
-            }
-            directions[arr[i]].points.forEach(
-                                    p=> {
-                                            arrayPoint.push(new Point(dx + x + p[0] * newSize * 0.7, dy + y + p[1] * newSize * 0.7))
-                                        }
-                                )
-        }
-
-        return arrayPoint; 
-    }
-
     function definitive(size,dx,dy,first){
         const arr = [];
         if(!order){
@@ -252,7 +123,7 @@ function App(){
 
     if(canvas){
         var ctx = canvas.current.getContext("2d");
-        const arrPoint = definitive(1002,10,10,'D');
+        const arrPoint = definitive(492,10,10,'D');
         ctx.lineWidth = 2;
         const inc = 360/Math.pow(4,order + 1);
         ctx.clearRect(0, 0, 1024, 1024);
@@ -266,6 +137,7 @@ function App(){
 
     }
 
+    console.log()
 
     return(
         <div
@@ -283,8 +155,8 @@ function App(){
             >Cambiar orden
             </button>
             <canvas
-                width="1024"
-                height="1024"
+                width="512"
+                height="512"
                 ref={canvasRef}
             />
         </div>
